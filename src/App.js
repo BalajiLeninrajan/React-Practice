5b40fb5328f7fcb88ddae1239e43424108cb8457
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-04-28 14:20:15 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const [enable, setEnable] = useState(false);
+
+    useEffect(() => {
+        let intervalId;
+
+        if (enable) {
+            intervalId = setInterval(() => {
+                setCounter(counter + 1);
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [counter, enable]);
+
+    const toggle = () => {
+        setEnable(!enable);
+    };
+
+    const reset = () => {
+        setEnable(false);
+        setCounter(0);
+    };
+
+    return (
+        <div className="App">
+            <h1>{counter}</h1>
+            <button onClick={toggle}>{enable ? "Stop" : "Start"}</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
 }
 
 export default App;
