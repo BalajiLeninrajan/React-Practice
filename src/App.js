0e6c8e4687647459629d42b4ed3e4319e651c402
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-09-18 22:03:20 -0400

    Reverted back to skeleton

--- src/App.js
+++ src/App.js
@@ -2,39 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [enable, setEnable] = useState(false);
-
-    useEffect(() => {
-        let intervalId;
-
-        if (enable) {
-            intervalId = setInterval(() => {
-                setCounter(counter + 1);
-            }, 1000);
-        }
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, [counter, enable]);
-
-    const toggle = () => {
-        setEnable(!enable);
-    };
-
-    const reset = () => {
-        setEnable(false);
-        setCounter(0);
-    };
-
-    return (
-        <div className="App">
-            <h1>{counter}</h1>
-            <button onClick={toggle}>{enable ? "Stop" : "Start"}</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
+  const [counter, setCounter] = useState(0);
+  const [enable, setEnable] = useState(false);
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={toggle}>{enable ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
 }
 
 export default App;
